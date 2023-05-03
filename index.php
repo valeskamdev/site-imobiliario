cf747ab09e8a0744a49d108befd80e01a15c0f31
valeskamdev <valeskkitty@gmail.com>
2023-05-03 20:32:46 -0300

    parametrizando projeto

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bootstrap Builder</title>
+    <link rel="stylesheet" href="_cdn/css/bootstrap_custom.css">
+    <link rel="stylesheet" href="_cdn/css/app.css">
+</head>
+<body>
+    <h1>Bootstrap Builder</h1>
+    <p>Projeto para construção de sites com Bootstrap</p>
+    <button class="btn btn-front icon-info">teste</button>
+
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
