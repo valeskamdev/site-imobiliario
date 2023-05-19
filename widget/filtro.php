53c2f09427f775442e972844a653513445d74f58
valeskamdev <valeskkitty@gmail.com>
2023-05-19 20:20:23 -0300

    Página filtro

--- widget/filtro.php
+++ widget/filtro.php
@@ -1,1 +1,145 @@
-FILTRO
+<div class="main_filter bg-light pt-5">
+    <div class="container">
+        <section class="row">
+            <div class="col-12">
+                <h2 class="text-front icon-filter mb-5">Filtro</h2>
+            </div>
+
+            <div class="col-12 col-md-5 col-lg-4">
+                <form action="" class="p-3 bg-white w-100 mb-5">
+                    <div class="row">
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou Alugar?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">O que você quer?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Qual o tipo do imóvel?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha.." multiple
+                                    data-actions-box="true">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Onde você quer?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="col-12 mt-3 text-right">
+                            <button class="btn btn-front icon-search">Pesquisar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-12 col-md-7 col-lg-8">
+                <section class="main_properties">
+                    <div class="container">
+                        <div class="row">
+                            <article class="col-12 col-md-12 col-lg-6 mb-4">
+                                <div class="card main_properties_item">
+                                    <div class="img-responsive-16by9">
+                                        <a href="#"><img src="properties/1/5a3571ab-4d76-466f-8246-eff8cb98cedd.jpg" class="card-img-top" alt=""></a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                                            <p class="text-muted">1</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                                            <p class="text-muted">4</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="col-12 col-md-12 col-lg-6 mb-4">
+                                <div class="card main_properties_item">
+                                    <div class="img-responsive-16by9">
+                                        <a href="#"><img src="properties/2/1cd48888-1311-4280-bcc3-39109c7d1875.jpg" class="card-img-top" alt=""></a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                                            <p class="text-muted">1</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                                            <p class="text-muted">4</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="col-12 col-md-12 col-lg-6 mb-4">
+                                <div class="card main_properties_item">
+                                    <div class="img-responsive-16by9">
+                                        <a href="#"><img src="properties/3/1c7275a0-c84e-4f25-9247-9b56664eb294.jpg" class="card-img-top" alt=""></a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                                            <p class="text-muted">1</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                                            <p class="text-muted">4</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_feature text-center">
+                                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </div>
+</div>
