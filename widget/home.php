835c1f275bc05979b38e87f98496e265b3ba0d43
valeskamdev <valeskkitty@gmail.com>
2023-05-18 21:19:59 -0300

    [Home] dobra listagem

--- widget/home.php
+++ widget/home.php
@@ -99,7 +99,7 @@
 <section class="main_list_group py-5 bg-light">
     <div class="container">
         <div class="border-bottom border-front p-4">
-            <h1 class="text-center">Ambiente no seu estilo <span class="text-front"><b>estilo</b></span></h1>
+            <h1 class="text-center">Ambiente no seu <span class="text-front"><b>estilo</b></span></h1>
             <p class="text-center text-muted h4 ">Encontre um imóvel com a experiência que você quer viver</p>
         </div>
 
@@ -150,6 +150,195 @@
                         <h2>Lojas e Salas</h2>
                     </div>
                 </a>
+            </article>
+        </div>
+    </div>
+</section>
+
+<section class="main_properties py-5">
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center border-bottom border-front mb-5">
+            <h2 class="text-front">À Venda</h2>
+            <a href="#" class="text-front">Ver mais</a>
+        </div>
+        <div class="row">
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="#"><img src="properties/1/5a3571ab-4d76-466f-8246-eff8cb98cedd.jpg" class="card-img-top" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer d-flex text-muted">
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="#"><img src="properties/2/1cd48888-1311-4280-bcc3-39109c7d1875.jpg" class="card-img-top" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer d-flex text-muted">
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="#"><img src="properties/3/1c7275a0-c84e-4f25-9247-9b56664eb294.jpg" class="card-img-top" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer d-flex text-muted">
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
+<section class="main_properties py-5 bg-light">
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center border-bottom border-front mb-5">
+            <h2 class="text-front">Para alugar</h2>
+            <a href="#" class="text-front">Ver mais</a>
+        </div>
+        <div class="row">
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="#"><img src="properties/4/3d656134-3760-4c9a-af1a-503301acc0be.jpg" class="card-img-top" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 4.000,00/mês</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer d-flex text-muted">
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="#"><img src="properties/5/8771b309-8314-4023-89a8-1f1b01165075.jpg" class="card-img-top" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 2.000,00/mês</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer d-flex text-muted">
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="#"><img src="properties/2/4941f006-f53b-4548-9a9f-fa5712bbfb1f.jpg" class="card-img-top" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="#" class="text-front">Linda Casa no Rio Tavares com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 1.500,00/mês</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer d-flex text-muted">
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="icone de uma cama">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid alt="icone de uma garagem">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_feature text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid alt="icone medindo área de uma casa">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
             </article>
         </div>
     </div>
